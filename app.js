172d21b8fb45460ce551506c3a4edc6943df538c
Eduard <nohstng@gmail.com>
2026-06-06 22:01:17 +0300

    Базовые арифметические операторы

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-let a = 1;
-console.log(a);
-a = 'test';
-console.log(a);
+// Базовые арифметические операторы
+
+const wight = 10;
+const height = 5;
+const space = wight * height;
+console.log(space);
+
+const newWight = wight - 4;
+const newWight2 = wight + 4;
+console.log(newWight);
+console.log(newWight2);
+
+const division = newWight / newWight2;
+console.log(division);
+
+const volume = 5 ** 3;  // 5 в степени 3
+console.log(volume);
+
+// Строки
+
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ', ' + street + ' ' + 5);
